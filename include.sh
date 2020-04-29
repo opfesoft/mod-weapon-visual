13954e4472d982fafb54e5743ac717c2f65d697b
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2020-04-30 00:35:41 +0200

    Remove conf.sh.dist

--- include.sh
+++ include.sh
@@ -2,8 +2,10 @@
 
 MOD_WEAPON_VISUAL_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
-source $MOD_WEAPON_VISUAL_ROOT"/conf/conf.sh.dist"
+DB_CHARACTERS_CUSTOM_PATHS+=(
+        $MOD_WEAPON_VISUAL_ROOT"/sql/characters/base"
+)
 
-if [ -f $MOD_WEAPON_VISUAL_ROOT"/conf/conf.sh" ]; then
-    source $MOD_WEAPON_VISUAL_ROOT"/conf/conf.sh"
-fi
+DB_WORLD_CUSTOM_PATHS+=(
+        $MOD_WEAPON_VISUAL_ROOT"/sql/world/base"
+)
